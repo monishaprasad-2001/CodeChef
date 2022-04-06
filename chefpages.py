8918a6fc35004e8ba12740112b9530d92e3b74ef
monishaprasad-2001 <85404517+monishaprasad-2001@users.noreply.github.com>
2022-04-06 21:03:06 +0530

    Create chefpages.py

---
+++ chefpages.py
@@ -0,0 +1,9 @@
+a=list(map(int,input().split(" ")))
+b = a[0]
+c = a[1]
+if b==1 and c==1:
+    print("https://discuss.codechef.com")
+elif b==0:
+    print("https://www.codechef.com/practice")
+elif b==1 and c==0:
+    print("https://www.codechef.com/contests")
